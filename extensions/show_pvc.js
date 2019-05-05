2bc130f308c1f93e142f3c80799e52f41aed852d
Viktor Kojouharov <developer@sugr.org>
2019-05-05 22:31:23 +0300

    Add a pvc extension

---
+++ extensions/show_pvc.js
@@ -0,0 +1,61 @@
+(function() {
+    function PVC() {
+        this.namespace = "";
+        this.names = [];
+    }
+
+    PVC.prototype.onObjectSelected = function(obj) {
+        if (sprintf("%T", obj).split(".")[1] == "Pod") {
+            this.names = [];
+            obj.Spec.Volumes.forEach(function(vol) {
+                if (vol.PersistentVolumeClaim != null) {
+                    this.names.push(vol.PersistentVolumeClaim.ClaimName)
+                }
+            }.bind(this))
+
+            if (!this.names.length) {
+                return null
+            }
+
+            this.namespace = obj.Namespace
+
+            return {"label": "Persistent Volume Claims", "cb": this.actionCallback.bind(this)}
+        }
+
+        return null
+    }
+
+    PVC.prototype.actionCallback = function() {
+        var name = this.names[0];
+
+        if (this.names.length > 1) {
+            // Displays a list dialog with the string array, returning the
+            // user-selected one
+            name = kd.Choose("Persistent Volume Claims", this.names)
+        }
+
+        // Client holds the k8s.Client object
+        var secret = kd.Client().CoreV1().PersistentVolumeClaims(this.namespace).Get(name, {})
+
+        // Display can show plain text, or a k8s object
+        kd.Display(secret)
+    }
+
+    PVC.prototype.del = function(obj) {
+        kd.Client().CoreV1().PersistentVolumeClaims(obj.Namespace).Delete(obj.Name, {"PropagationPolicy": "Foreground"})
+    }
+
+    PVC.prototype.update = function(obj) {
+        kd.Client().CoreV1().PersistentVolumeClaims(obj.Namespace).Update(obj, {})
+    }
+
+    var pvc = new PVC()
+
+    // Callback for when an object is selected in the tree
+    // Return null if no action is to be taken.
+    // Return ["Action name", callback] otherwise.
+    kd.RegisterActionOnObjectSelected(pvc.onObjectSelected.bind(pvc))
+
+    // Register callbacks that deal with object mutation of a certain type
+    kd.RegisterObjectMutateActions("PersistentVolumeClaim", {"delete": pvc.del.bind(pvc), "update": pvc.update.bind(pvc)})
+})()
