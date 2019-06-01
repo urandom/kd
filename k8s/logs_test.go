a0636a83d60ccd8c45e74c31446e35a54b1adb95
Viktor Kojouharov <developer@sugr.org>
2019-06-01 17:02:51 +0300

    Add some tests for the logs

--- k8s/logs_test.go
+++ k8s/logs_test.go
@@ -1,31 +1,58 @@
 package k8s_test
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"testing"
+	"time"
 
 	"github.com/urandom/kd/k8s"
+	av1 "k8s.io/api/apps/v1"
+	cv1 "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	rest "k8s.io/client-go/rest"
 )
 
 func TestClient_Logs(t *testing.T) {
-	type args struct {
-		ctx       context.Context
+	tests := []struct {
+		name      string
+		timeout   time.Duration
 		object    k8s.ObjectMetaGetter
 		container string
 		colors    []string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    <-chan []byte
-		wantErr bool
+		want      [][]byte
+		wantErr   bool
 	}{
-		// TODO: Add test cases.
+		{name: "no valid object"},
+		{name: "no pods in object", object: k8s.NewCtrlWithPods(&av1.Deployment{}, k8s.CategoryDeployment, nil, nil)},
+		{name: "pod in object - timeout", object: k8s.NewCtrlWithPods(&av1.Deployment{}, k8s.CategoryDeployment, nil, []*cv1.Pod{
+			{ObjectMeta: meta.ObjectMeta{Name: "pod1"}, Status: cv1.PodStatus{
+				ContainerStatuses: []cv1.ContainerStatus{{Name: "con1"}},
+			}},
+		}), colors: []string{"red", "blue"}, timeout: 100 * time.Millisecond},
+		{name: "pod - timeout", object: &cv1.Pod{
+			ObjectMeta: meta.ObjectMeta{Name: "pod1"}, Status: cv1.PodStatus{
+				ContainerStatuses: []cv1.ContainerStatus{{Name: "con1"}},
+			},
+		}, colors: []string{"red", "blue"}, timeout: 100 * time.Millisecond},
+		{name: "multi container - timeout", object: &cv1.Pod{
+			ObjectMeta: meta.ObjectMeta{Name: "pod1"}, Status: cv1.PodStatus{
+				ContainerStatuses: []cv1.ContainerStatus{{Name: "con1"}, {Name: "con2", LastTerminationState: cv1.ContainerState{Terminated: &cv1.ContainerStateTerminated{}}}},
+			},
+		}, colors: []string{"red", "blue"}, wantErr: true},
+		{name: "multi container pod specified - timeout", object: &cv1.Pod{
+			ObjectMeta: meta.ObjectMeta{Name: "pod1"}, Status: cv1.PodStatus{
+				ContainerStatuses: []cv1.ContainerStatus{{Name: "con1"}, {Name: "con2", LastTerminationState: cv1.ContainerState{Terminated: &cv1.ContainerStateTerminated{}}}},
+			},
+		}, container: "con2", colors: []string{"red", "blue"}, timeout: 100 * time.Millisecond},
+		{name: "multi container pod specified previous - timeout", object: &cv1.Pod{
+			ObjectMeta: meta.ObjectMeta{Name: "pod1"}, Status: cv1.PodStatus{
+				ContainerStatuses: []cv1.ContainerStatus{{Name: "con1"}, {Name: "con2", LastTerminationState: cv1.ContainerState{Terminated: &cv1.ContainerStateTerminated{}}}},
+			},
+		}, container: "previous:con2", colors: []string{"red", "blue"}, timeout: 100 * time.Millisecond},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -43,14 +70,37 @@ func TestClient_Logs(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			got, err := c.Logs(tt.args.ctx, tt.args.object, tt.args.container, tt.args.colors)
+			got, err := c.Logs(timeoutAfter(tt.timeout), tt.object, tt.container, tt.colors)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Client.Logs() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Client.Logs() = %v, want %v", got, tt.want)
+			if got == nil && tt.want == nil {
+				return
+			}
+			for data := range got {
+				var found bool
+				for i := range tt.want {
+					if bytes.Equal(data, tt.want[i]) {
+						found = true
+					}
+				}
+
+				if !found {
+					t.Errorf("Client.Logs() = %v, want %v", data, tt.want)
+				}
 			}
 		})
 	}
+}
+
+func timeoutAfter(d time.Duration) context.Context {
+	if d == 0 {
+		return context.Background()
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	time.AfterFunc(d, cancel)
+
+	return ctx
 }
